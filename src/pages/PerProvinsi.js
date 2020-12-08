2df3178b975eff866f8f1390348677741766f64d
yeheskielwonte <yeheskielwonte05@gmail.com>
2020-12-08 14:20:50 +0800

    Get API provinsi

--- src/pages/PerProvinsi.js
+++ src/pages/PerProvinsi.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState  } from 'react'
+import axios from 'axios'
 
-const PerProvinsi = () =>{
-    return (
-        <div>
-            <h1>PerProvinsi</h1>
-        </div>
+const Provinsi = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get("https://indonesia-covid-19.mathdro.id/api/provinsi")
+        .then((response) => {setData(response.data.data)});
+    }, [])
+
+return (
+    <div>
+    {
+        data.map((items) => {
+            return(
+                <div>
+                    <p>{items.provinsi}</p>
+                    <p>{items.kasusPosi}</p>
+                    <p>{items.kasusSemb}</p>
+                    <p>{items.kasusMeni}</p>
+                </div>
+        )
+    })
+}
+</div>
     )
 }
 
-export default PerProvinsi;
+export default Provinsi;
